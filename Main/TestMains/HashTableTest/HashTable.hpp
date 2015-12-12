f72ae1e5fc8110b846822d7a66858e9aa6e38dca
JimR21 <mitsos-r21@hotmail.gr>
2015-12-12 03:31:50 +0200

    HashTable delete update

--- Main/TestMains/HashTableTest/HashTable.hpp
+++ Main/TestMains/HashTableTest/HashTable.hpp
@@ -12,7 +12,7 @@ class HashTable
 {
     friend class Bucket;
 private:
-    int maxLocalCounter;    // Posa buckets exoun localDepth = globalDepth
+    DArray<int> maxLocalCounter;    // Posa buckets exoun localDepth = globalDepth
 
     unsigned size;
 
@@ -37,9 +37,9 @@ public:
 
     void insert(unsigned key, unsigned int tid, unsigned int offset);
 
-	DArray<int>* getHashRecord(unsigned key, uint64_t start_tid, uint64_t end_tid);
+	DArray<uint64_t>* getHashRecord(unsigned key, uint64_t start_tid, uint64_t end_tid);
 
-	DArray<int>* getHashRecords(unsigned key);
+	DArray<uint64_t>* getHashRecords(unsigned key);
 
 	bool existCheck(unsigned key,uint64_t, uint64_t);
 
