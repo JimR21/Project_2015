4d39b64f5492bbcc0e66c5a1b1ee68ad1f2ae322
p1ranhas <sdi1100006@di.uoa.gr>
2016-01-11 12:46:16 +0200

    DArray Test + update run.sh

---
+++ Main/TestMains/DArrayTest/JournalRecord.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdint.h>
+#include "DArray.hpp"
+
+#define DELETE false
+#define INSERT true
+
+class JournalRecord {
+
+private:
+    uint64_t transaction_id;
+    uint64_t* values;
+	bool type;
+
+public:
+    JournalRecord(uint32_t);
+    JournalRecord(uint64_t, bool, uint32_t);
+	~JournalRecord();
+
+    JournalRecord(const JournalRecord &rec, uint64_t newtid, uint32_t columns);   // Copy constractor for DELETE records
+
+	bool getType();
+	uint64_t getValue(uint32_t);
+	uint64_t getTransactionId();
+	uint64_t*getAllValues();
+
+	void addValue(uint64_t, uint32_t);
+	void printRecord();
+};
--- Main/TestMains/run.sh
+++ Main/TestMains/run.sh
@@ -8,6 +8,7 @@ echo
 echo "Option[1]: JournalRecord Test Main"
 echo "Option[2]: Journal Test Main"
 echo "Option[3]: HashTable Test"
+echo "Option[4]: DArray Test"
 
 read option
 
@@ -17,6 +18,8 @@ elif [ $option == 2 ]; then
     g++ JournalTest/JournalTestMain.cpp JournalTest/JournalRecord.cpp Journal.cpp
 elif [ $option == 3 ]; then
 	g++ -std=c++0x HashTableTest/Bucket.cpp HashTableTest/BucketData.cpp HashTableTest/HashTable.cpp HashTableTest/HashTestMain.cpp
+elif [ $option == 4 ]; then
+    g++ -std=c++0x DArrayTest/DArrayTest.cpp DArrayTest/JournalRecord.cpp
 fi
 
 ./a.out
